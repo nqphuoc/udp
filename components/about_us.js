935955ad0ca640bd38323397d5d0ee0b344a66a5
nqphuoc <nqphuoc@gmail.com>
2021-01-01 05:23:48 +0900

    about US

--- components/about_us.js
+++ components/about_us.js
@@ -13,20 +13,12 @@ const AboutUs = () => {
                 <SectionHeader name='about us' align='left' />
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <h3>Services</h3>
-                <div className='ul'>
-                    <ul>
-                        <li>Lorem ipsum dolor</li>
-                        <li>Tempor incididunt</li>
-                        <li>Lorem ipsum dolor</li>
-                        <li>Incididunt ut labore</li>
-                    </ul>
-                    <ul>
-                        <li>Aliquip ex ea commodo</li>
-                        <li>Lorem ipsum dolor</li>
-                        <li>Exercitation ullamco</li>
-                        <li>Lorem ipsum dolor</li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>Lorem ipsum dolor</li>
+                    <li>Tempor incididunt</li>
+                    <li>Lorem ipsum dolor</li>
+                    <li>Incididunt ut labore</li>
+                </ul>
                 <h3>Solutions</h3>
                 <div className='ul'>
                     <ul>
