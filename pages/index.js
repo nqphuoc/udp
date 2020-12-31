be1e1e9da6f957efce1468ed0e4cd4eb80109d48
nqphuoc <nqphuoc@gmail.com>
2021-01-01 04:45:15 +0900

    remove tech

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 import AppNavigator from '../components/navigator';
 import Intro from '../components/intro';
-import CoreTechnology from '../components/core_technology';
+//import CoreTechnology from '../components/core_technology';
 import AboutUs from '../components/about_us';
 import Solutions from '../components/solutions';
 import Team from '../components/team';
@@ -15,14 +15,14 @@ export default function Home() {
   const [visibleSection, setVisibleSection] = useState();
 
   const headerRef = useRef(null);
-  const technologyRef = useRef(null);
+  //const technologyRef = useRef(null);
   const aboutRef = useRef(null);
   const solutionRef = useRef(null);
   const teamRef = useRef(null);
   const contactRef = useRef(null);
 
   const sectionRefs = [
-    {section: 'technology', ref: technologyRef},
+    //{section: 'technology', ref: technologyRef},
     {section: 'about', ref: aboutRef},
     {section: 'solutions', ref: solutionRef},
     {section: 'team', ref: teamRef},
@@ -69,7 +69,7 @@ export default function Home() {
     const { height } = ele.getBoundingClientRect();
     const offsetTop = ele.offsetTop;
     const offsetBottom = offsetTop + height;
-  
+
     return {
       height,
       offsetTop,
@@ -89,9 +89,9 @@ export default function Home() {
   const scrollToSection = (section) => {
 
     switch(section){
-      case 'technology':
-        scrollTo(technologyRef.current);
-        break;
+//      case 'technology':
+//        scrollTo(technologyRef.current);
+//        break;
 
       case 'about':
         scrollTo(aboutRef.current);
@@ -123,10 +123,10 @@ export default function Home() {
       <main>
         <Intro />
 
-        <div ref={technologyRef} className='feature' style={{backgroundColor: '#f6f6f6', textAlign: 'center'}}>
-          <SectionHeader name='Core Technology' />
-          <CoreTechnology />
-        </div>
+//        <div ref={technologyRef} className='feature' style={{backgroundColor: '#f6f6f6', textAlign: 'center'}}>
+//          <SectionHeader name='Core Technology' />
+//          <CoreTechnology />
+//        </div>
 
         <div ref={aboutRef} className='feature'>
           <AboutUs />
@@ -138,13 +138,13 @@ export default function Home() {
         </div>
 
         <div ref={teamRef} className='feature' style={{textAlign: 'center'}}>
-          <SectionHeader name='meat the team' /> 
+          <SectionHeader name='meat the team' />
           <Team />
         </div>
       </main>
 
       <footer ref={contactRef}>
-        <Footer />  
+        <Footer />
       </footer>
 
       <style jsx>{`
