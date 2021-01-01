1bbda57a26a3815c55ad1985b314c3b2ccd4e617
nqphuoc <nqphuoc@gmail.com>
2021-01-02 05:52:42 +0900

    intro

--- components/about_us.js
+++ components/about_us.js
@@ -11,64 +11,16 @@ const AboutUs = () => {
             </div>
             <div className='about_detail' style={{flex: 2, display: 'flex', flexDirection: 'column', fontSize: 15}}>
                 <SectionHeader name='about us' align='left' />
-                <p>Established in Seoul, S. Korea in 2020, UDP Sol (User Data Processing Solution) is an entrepreneur gathering Data Scientists and Natural Language Processing Specialists.</p>
-                <p>The mission of UDP Sol is to provide services and solutions to benefit customers in three aspects.</p>
+                <p>Established in Seoul, S. Korea in 2020, UDP Sol (User Data Processing Solution) is an entrepreneur gathering Data Scientists and Natural Language Processing Specialists. The mission of UDP Sol is to provide NLP-based AI services and solutions to benefit customers in three aspects. Recently, UDP Sol has supported English, Korean, and Vietnamese languages. </p>
                 <h3>Insightful Benefits</h3>
                 <div className='ul'>
                     <ul>
-                        <li>Reduce Costs from Day One</li>
-                    </ul>
-                    <ul>
-                        <li>Increase Customer Satisfaction</li>
-                    </ul>
-                    <ul>
-                        <li>Boost Employee Productivity</li>
+                    <li>Reduce Costs from Day One</li>
+                    <li>Increase Customer Satisfaction</li>
+                    <li>Boost Employee Productivity</li>
                     </ul>
                 </div>
-                <p>UDP Sol has recently supported English, Korean, and Vietnamese languages and focuses on developing the following services and solutions.</p>
-                <h3>Language Resources (Corpora)</h3>
-                <div className='ul'>
-                    <ul>
-                        <li>Unlabeled Vietnamese Corpus (15 Billions sentences-100GB)</li>
-                    </ul>
-                    <ul>
-                        <li>English-Korean Parallel Corpus (1.25 Million sentence pairs)</li>
-                    </ul>
-                    <ul>
-                        <li>Korean-Vietnamese Parallel Corpus (450K sentence pairs)</li>
-                    </ul>
-                </div>
-
-                <h3>Services</h3>
-                <div className='ul'>
-                    <ul>
-                        <li>Automatic Data Annotation API</li>
-                    </ul>
-                    <ul>
-                        <li>English-Korean Parallel Corpus (1.25 Million sentence pairs)</li>
-                    </ul>
-                    <ul>
-                        <li>Korean-Vietnamese Parallel Corpus (450K sentence pairs)</li>
-                    </ul>
-                </div>
-
-                <h3>Solutions</h3>
-                <div className='ul'>
-                    <ul>
-                        <li>Reduce Costs from Day One</li>
-                        <li>Increase Customer Satisfaction</li>
-                        <li>Boost Employee Productivity</li>
-                        <li>Incididunt ut labore</li>
-                    </ul>
-                    <ul>
-                        <li>Aliquip ex ea commodo</li>
-                        <li>Lorem ipsum dolor</li>
-                        <li>Exercitation ullamco</li>
-                        <li>Lorem ipsum dolor</li>
-                    </ul>
-                </div>
-
-            </div>
+          </div>
             <style jsx>
             {
                 `
